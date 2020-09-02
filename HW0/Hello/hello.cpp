b2f93daed977a879ac6f70f65a030f94b440f13c
Jasmine Kobayashi <rokkakobayashi@msn.com>
2020-09-01 22:17:47 -0800

    HW0 main program

---
+++ HW0/Hello/hello.cpp
@@ -0,0 +1,15 @@
+// File name:
+// Author: Jasmine Kobayashi
+// Date: 8/31/2020
+// Description: Program to print "Hello, World!" five times
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	for (int i = 0; i < 5; i = i + 1)
+		cout << "Hello, World!" << endl;
+	return 0;
+}
