71efcfcb206dda706d1b138796ca0f91987be623
Jasmine Kobayashi <rokkakobayashi@msn.com>
2020-09-02 23:54:44 -0800

    (1st finished) Program to print description of favorite game.

---
+++ HW0/Favorite Game/fav_game.cpp
@@ -0,0 +1,18 @@
+// File name: fav_game.cpp
+// Author: Jasmine Kobayashi
+// Date: 9/2/2020
+// Description: Program that prints a description of my favorite game (Portal)
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "One of my most favorite games is a game called \'Portal\'." << endl;
+	cout << " \'Portal\' is a puzzle game where you continue through the story by completing different \'testing rooms\'." ;
+	cout << " Each of the puzzles are done with a portal gun, and the objective is to reach the exit of the \'testing rooms\'." ;
+	cout << " There is also some sense of a boss fight at the end of the game.";
+	cout << " And main story progresses with the 2nd game (\'Portal 2\')." << endl;
+	return 0;
+}
