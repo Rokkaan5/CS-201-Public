c535880abdc1bc9d895cb2d5bec137e9929b0940
Jasmine Kobayashi <rokkakobayashi@msn.com>
2020-09-02 20:31:20 -0800

    Add "Additional Program 1" of HW0 (poem exerpt) to HW0 in repo

---
+++ HW0/Poem/poem.cpp
@@ -0,0 +1,16 @@
+// File name: poem.cpp
+// Author: Jasmine Kobayashi
+// Date: 9/2/2020
+// Description: Program that prints an exerpt from a poem ("Stoppin by Woods on a Snowy Evening" by Robert Frost).
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "The only other sound's the sweep \nOf easy wind and downy flake." << endl;
+	cout << "The woods are lovely, dark and deep, \nBut I have promises to keep, \nAnd miles to go before I sleep," << endl;
+	cout << "And miles to go before I sleep." << endl;
+	return 0;
+}
