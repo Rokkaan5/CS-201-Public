ba7a6a763287c3ef6202d50065173104da99dcb1
Jasmine Kobayashi <rokkakobayashi@msn.com>
2020-09-09 10:39:19 -0800

    Lab4 with "Additional" programs (as well as "required")

--- Labs/Lab4/Lab4.cpp
+++ Labs/Lab4/Lab4.cpp
@@ -1,7 +1,7 @@
 // File name: Lab4.cpp
 // Author: Jasmine Kobayashi
-// Date: 9/9/2020
-// Description: Program to print numbers 1-100, and prints a string at certain numbers divisible by a certain number
+// Date: 9/8/2020
+// Description: Program to print numbers 1-100, and prints statement(s) at certain numbers divisible by a certain number(s)
 
 #include <iostream>
 
@@ -9,12 +9,34 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-int main()
+int required(int div)
 {
 	for (int i = 0; i <= 100; ++i)
-		if (i % 3 == 0)
+		if (i % div == 0)
 			cout << "Fizz" << endl;
 		else
 			cout << i << endl;
+	return 0;
+}
+
+int additional(int div1, int div2)
+{
+	for (int i = 1; i <= 100; ++i)
+		if (i % div1 == 0 and i % div2 == 0)
+			cout << "Fizz Buzz" << endl;
+		else if (i % div1 == 0)
+			cout << "Fizz" << endl;
+		else if (i % div2 == 0)
+			cout << "Buzz" << endl;
+		else
+			cout << i << endl;
+	return 0;
+}
+
+int main()
+{
+	//required(3);
+	additional(3,5);
+
 	return 0;
 }
