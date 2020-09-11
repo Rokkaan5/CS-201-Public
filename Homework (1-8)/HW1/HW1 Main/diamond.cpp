9e7ce56af1f89596f575d7001243ae45146f5ed3
Jasmine Kobayashi <rokkakobayashi@msn.com>
2020-09-11 12:10:47 -0800

    HW1 Main program, basic necessities

---
+++ Homework (1-8)/HW1/HW1 Main/diamond.cpp
@@ -0,0 +1,10 @@
+// File name: diamond.cpp
+// Author: Jasmine Kobayashi
+// Date: 9/11/20
+// Description: A program that inputs a positive interger from a user, and prints a correspondingly sized diamond made of pound signs
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
