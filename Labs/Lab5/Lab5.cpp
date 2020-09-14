cd1953f38043b0a4878fbd759e2d31ca5ba8c017
Jasmine Kobayashi <rokkakobayashi@msn.com>
2020-09-14 00:09:32 -0800

    Add 'Lab5' to 'Labs' in repo

---
+++ Labs/Lab5/Lab5.cpp
@@ -0,0 +1,9 @@
+// File Name: Lab5.cpp
+// Author: Jasmine Kobayashi
+// Date: 9/13/2020
+// Description: Program that uses switch and does one of three things when a number is inputted
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
