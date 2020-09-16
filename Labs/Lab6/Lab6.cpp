7b9e970261c6c73f672fdb3927bd2f4afc367363
Jasmine Kobayashi <rokkakobayashi@msn.com>
2020-09-15 23:37:26 -0800

    Add Lab6 to Labs in repo

---
+++ Labs/Lab6/Lab6.cpp
@@ -0,0 +1,17 @@
+// File Name: Lab6.cpp
+// Author: Jasmine Kobayashi
+// Date: 9/15/2020
+// Description: A program that utilizes a function that takes a string as a parameter and outputs that string
+
+#include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+
+int main()
+{
+	return 0;
+}
