10086d713c54a837fa3e6adc2023f226e9bc3ad0
Jasmine Kobayashi <rokkakobayashi@msn.com>
2020-10-15 02:00:09 -0800

    Lab7 first finish

--- Labs/Lab7/Lab7.cpp
+++ Labs/Lab7/Lab7.cpp
@@ -4,8 +4,25 @@
 // Description:
 
 #include <iostream>
+#include "intio.hpp"
+using std::cout;
+using std::endl;
+
 
 int main()
 {
+	cout << "Type an integer(1/3): ";
+	int num1 = getInt();
+	cout << "Type another integer(2/3): ";
+	int num2 = getInt();
+	cout << "Type another integer(3/3): ";
+	int num3 = getInt();
+
+	putInt(num1, 10);
+	cout << endl;
+	putInt(num2, 10);
+	cout << endl;
+	putInt(num3, 10);
+	cout << endl;
 	return 0;
 }
