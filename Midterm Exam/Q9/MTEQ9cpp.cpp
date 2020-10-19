4d997206f02067d1976881f01362356554fae5ce
Jasmine Kobayashi <rokkakobayashi@msn.com>
2020-10-18 23:28:10 -0800

    Midterm Q9 Finalized (w/ testing)

--- Midterm Exam/Q9/MTEQ9cpp.cpp
+++ Midterm Exam/Q9/MTEQ9cpp.cpp
@@ -5,32 +5,41 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 using std::cout;
 using std::endl;
 using std::string;
+using std::vector;
 
 int PrintSubStrings(string stri)
 {
 	for (int i = 0; i < stri.size(); ++i)
 	{
-		cout << stri[i] << ' ';
+		for (int j = 0; j < stri.size()-i; ++j)
+		{
+			cout << stri.substr(j,i+1) << ' ';
+		}
+		cout << endl;
 	}
-	cout << endl;
-	for (int j = 0; j < stri.size() - 1; ++j)
-	{
-		cout << stri[j] << stri[j + 1] << ' ';
-	}
-	cout << endl;
 	return 1;
 }
 
+
 int main()
 {
-	string s = "abc";
-	string s2 = "Sakura";
+	string s1 = "abc";
+	string s2 = "Exam";
+	string s3 = "Miderm";
+	string s4 = "Computer";
+	string s5 = "Science";
 
-	PrintSubStrings(s);
-	PrintSubStrings(s2);
+	vector<string> v = { s1,s2,s3,s4,s5 };
+
+	for (int l = 0; l < v.size(); ++l)        //Testing
+	{
+		PrintSubStrings(v[l]);
+	}
+
 
 	return 0;
 }
