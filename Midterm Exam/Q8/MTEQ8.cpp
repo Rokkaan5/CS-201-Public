3f4d3d618dfd6894f466028e5e99bf66a3f3df79
Jasmine Kobayashi <rokkakobayashi@msn.com>
2020-10-18 21:42:50 -0800

    Midterm Q8 Finalized (w/ testing in main)

--- Midterm Exam/Q8/MTEQ8.cpp
+++ Midterm Exam/Q8/MTEQ8.cpp
@@ -23,6 +23,19 @@ bool isSorted(vector<int> vec)
 
 int main()
 {
-	
+	vector <int> v1 = { 1,2,3,4 };          //true
+	vector <int> v2 = { 3,1,4,5 };          //false
+	vector <int> v3 = { 4,6,8,9 };          //true
+	vector <int> v4 = { 7,9,12,25,21,34 };  //false
+	vector <int> v5 = { 11,21,16,14,18 };   //false
+	vector <int> v6 = { 5,6,9,10,15,39 };   //true
+
+	cout << std::boolalpha;
+	vector<vector<int>> v = { v1,v2,v3,v4,v5,v6 };
+	for (auto j = 0; j < v.size(); ++j)
+	{
+		cout << isSorted(v[j]) << endl;
+	}
+
 	return 0;
 }
