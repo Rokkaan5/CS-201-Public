6976ea7e4a436359f437ba574a3542d18dee84fd
Jasmine Kobayashi <rokkakobayashi@msn.com>
2020-10-18 20:44:22 -0800

    Q7 print_range function

---
+++ Midterm Exam/Q7/MTEQ7.cpp
@@ -0,0 +1,26 @@
+// File Name: MTEQ7.cpp      (Midterm Exam Question 7)
+// Author: Jasmine Kobayashi
+// Date: 10/18/20
+// Description: A function that takes two integers and prints each integer in the range between them. 
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+
+int print_range(int x, int y)
+{
+	for (int i = x + 1; i < y; ++i)
+	{
+		cout << i << ' ';
+	}
+	cout << endl;
+	return 1;
+}
+
+
+int main()
+{
+	print_range(3, 8);
+	return 0;
+}
